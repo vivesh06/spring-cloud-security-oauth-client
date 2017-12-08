36e9b3cbb6b1e0a62f9d1351ef0ddd7346e9e96d
vivesh <vivesh06@gmail.com>
2017-12-08 10:06:47 +0200

    dev

--- src/main/java/com/learning/oauth/client/HomeController.java
+++ src/main/java/com/learning/oauth/client/HomeController.java
@@ -1,14 +1,29 @@
-package com.learning.oauth.client;
+	package com.learning.oauth.client;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @EnableOAuth2Sso
 public class HomeController extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private OAuth2ClientContext context;
+	
+	@Autowired
+	private OAuth2RestTemplate template;
 
 	@RequestMapping("/")
 	public String home() {
@@ -16,8 +31,24 @@ public class HomeController extends WebSecurityConfigurerAdapter {
 	}
 	
 	@RequestMapping("/test")
-	public String test() {
+	public String test(Model model) {
+		System.out.println("ACC TOken:" + context.getAccessToken().getValue());
+		ResponseEntity<List<Data>> responseEntity = template.exchange("http://localhost:8083/services/reports", HttpMethod.GET, null, new ParameterizedTypeReference<List<Data>>(){});
+		model.addAttribute("data", responseEntity.getBody());
 		return "test";
+	}
+	
+	public static class Data {
+		
+		public String name;
+		
+		public Data() {}
+
+		public Data(String name) {
+			super();
+			this.name = name;
+		}
+		
 	}
 	
 	@Override
